4b8e78dbabd97fb3cceffcefb823ed25e9406e93
Louis DeScioli <louis.descioli@gmail.com>
2016-05-20 19:15:22 -0400

    Adds some state to the component in the form of button clicks

--- client/component.jsx
+++ client/component.jsx
@@ -5,9 +5,25 @@ let HelloWorld = React.createClass({
     name: React.PropTypes.string.isRequired,
   },
 
+  getInitialState() {
+    return {
+      clicks: 0,
+    }
+  },
+
+  onClick() {
+    this.setState({
+      clicks: this.state.clicks + 1,
+    })
+  },
+
   render() {
     return (
-      <h1>Hello {this.props.name}</h1>
+      <div>
+        <h1>Hello {this.props.name}</h1>
+        <h2>You've clicked the button {this.state.clicks} times</h2>
+        <button onClick={this.onClick}>Button</button>
+      </div>
     )
   },
 })
